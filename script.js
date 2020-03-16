edd990ceeb81ecf33aba6ee3c14b0498566a352c
Dalton Thompson <daltoncjthompson@aim.com>
2020-03-16 17:24:47 -0400

    Change to goal.innerText, global let statement for interval

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ let now = document.getElementsByTagName('h2')[0];
 let goal = document.getElementsByTagName('h3')[0];
 let title = document.getElementsByTagName('title')[0];
 let horse = document.getElementById("horseSound");
+horse.play();
+let horseInterval;
 
 // get desired interval from user
 function getWorkTime() {
     let userTime = Number(prompt("How many minutes would you like to work?"));
     while (isNaN(userTime) || userTime<= 0) {
-      alert("Sorry, you did not enter a valid number.");
-      userTime = Number(prompt("How many minutes would you like to work?"));
+        alert("Sorry, you did not enter a valid number.");
+        userTime = Number(prompt("How many minutes would you like to work?"));
     }
     return userTime
 }
@@ -19,7 +21,7 @@ let timeElapsed = 0;
 let goalpost = new Date();
 let currentTime = new Date();
 let timerLastUpdated = new Date();
-now.innerHTML = calculateTime(currentTime);
+now.innerText = calculateTime(currentTime);
 
 let workState = false;
 let tally = {'work': 0, 'stretch': 0, 'earned':0};
@@ -35,48 +37,51 @@ let interval = setInterval(() => {
         tally.earned -= elapsed/1000;
     }
 
-    now.innerHTML = calculateTime(currentTime);
+    now.innerText = calculateTime(currentTime);
         
-    document.querySelector('#tally-work').innerHTML = 
+    document.querySelector('#tally-work').innerText = 
         Math.floor(tally.work/60/60) + ':' + 
         prependZero(Math.floor(tally.work/60)) + ':' + 
         prependZero(Math.floor(tally.work%60));
 
-    document.querySelector('#tally-break').innerHTML =
+    document.querySelector('#tally-break').innerText =
         Math.floor(tally.stretch/60/60) + ':' +
         prependZero(Math.floor(tally.stretch/60)) + ':' +
         prependZero(Math.floor(tally.stretch%60));
 
     if (Math.sign(tally.earned) >= 0) {
-        document.querySelector('#saved-label').innerHTML = "Saved break time:";
+        document.querySelector('#saved-label').innerText = "Saved break time:";
     } else {
-        document.querySelector('#saved-label').innerHTML = "Overused break time:";
+        document.querySelector('#saved-label').innerText = "Overused break time:";
     };
-    document.querySelector('#saved-break').innerHTML = 
+    document.querySelector('#saved-break').innerText = 
         prependZero(Math.floor(Math.abs(tally.earned)/60/60)) + ':' + 
         prependZero(Math.floor(Math.abs(tally.earned)/60)) + ':' + 
         prependZero(Math.floor(tally.earned%60));
-    timerLastUpdated = currentTime;
-
-    // if (tally.earned < 0 && workState == false) horse.play()
-    
+    timerLastUpdated = currentTime;    
 },1000);
 
 function selectState(boolean) {
     if (boolean == true) {
         workState = true;
-        let workTime = getWorkTime()
-        setTimeout(function(){ horse.play(); }, (workTime*60)*1000 - 3000);
+        clearInterval(horseInterval);
+        let workTime = getWorkTime();
+        setTimeout(function(){
+            horse.play();
+            goal.innerText = 'You can take a break now.';
+        }, (workTime*60)*1000 - 3000);
         // play sound once reach to the work time goal
         goalpost.setMinutes(currentTime.getMinutes() + workTime);
-        goal.innerHTML = 'Alright, back to work! Try to keep going until ' + calculateTime(goalpost) + '. Then, take a minute to plan your next task before you take a break.';
+        goal.innerText = 'Alright, back to work! Try to keep going until ' + calculateTime(goalpost) + '. Then, take a minute to plan your next task before you take a break.';
     } else {
         workState = false;
         goalpost.setMinutes(timerLastUpdated.getMinutes() + 4);
         // play sound every two minutes
-        setInterval(function(){ horse.play(); }, 120000);
-        goal.innerHTML = 'Let\'s make this short. Walk around until ' + calculateTime(goalpost) + '!';
-
+        horseInterval = setInterval(function(){
+            horse.play();
+            goal.innerText = 'Back to work!';
+        }, 120000);
+        goal.innerText = 'Let\'s make this short. Walk around until ' + calculateTime(goalpost) + '!';
     };
 }
 
@@ -91,13 +96,16 @@ function calculateTime(input) {
     return input.getHours() + ':' + prependZero(input.getMinutes()) + ':' + prependZero(input.getSeconds());
 }
 
+//Code meant to fix an error with adding minutes that rolled over the hour mark.
+/*
 let addTime = (timeSource,addedValue) => {
     if ((timeSource.getMinutes() + addedValue) > 59) {
         return timeSource.getMinutes() + addedValue - 60;
     } else {
         return timeSource.getMinutes() + addedValue;
     }
-}
+}*/
 
+selectState(false);
 work.addEventListener("click", event => selectState(true));
 stretch.addEventListener("click", event => selectState(false));
